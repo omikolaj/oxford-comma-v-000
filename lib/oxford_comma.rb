f1cc2b2e02a3f63e9510acb0505976890339db05
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-01 10:24:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if(array.size == 1)
+    return array[0]
+  elsif (array.size == 2)
+    "#{array[0]} and #{array[1]}"
+  elsif (array.size == 3)
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    message = "";
+    counter = 0
+    until counter == (array.size - 1)
+      message += "#{array[counter]}, "
+      counter += 1
+    end
+    "#{message}and #{array[array.size-1]}"
+  end
 end
